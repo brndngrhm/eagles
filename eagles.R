f30ba537e7fa8873634f784280d1010aceaecf2c
brndngrhm <brendanvgraham@gmail.com>
2016-05-25 11:15:31 -0400

    load roster

--- eagles.R
+++ eagles.R
@@ -12,3 +12,20 @@ season <- agg_player_season(2015)
 names(season) <-  tolower(names(season))
 season <- season %>% filter(team == "PHI")
 players <- season %>% select(name, playerid)
+
+#get roster, assign players to offense, defense, st
+roster.2016x <- getURL("https://raw.githubusercontent.com/brndngrhm/eagles/master/roster_2016.csv")
+roster <- as.data.frame(read.csv(text=roster.2016x, strip.white = T))
+roster$mode <- "defense"
+roster$mode[roster$pos == "C"] <- "offense"
+roster$mode[roster$pos == "OG"] <- "offense"
+roster$mode[roster$pos == "OT"] <- "offense"
+roster$mode[roster$pos == "QB"] <- "offense"
+roster$mode[roster$pos == "WR"] <- "offense"
+roster$mode[roster$pos == "TE"] <- "offense"
+roster$mode[roster$pos == "T"] <- "offense"
+roster$mode[roster$pos == "G"] <- "offense"
+roster$mode[roster$pos == "RB"] <- "offense"
+roster$mode[roster$pos == "k"] <- "st"
+roster$mode[roster$pos == "p"] <- "st"
+
