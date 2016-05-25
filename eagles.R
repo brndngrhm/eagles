77632cef69074d28458e9e97ec51e3d7378cb406
brndngrhm <brendanvgraham@gmail.com>
2016-05-25 15:00:45 -0400

    added example

--- eagles.R
+++ eagles.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(nflscrapR)
 library(highcharter)
 library(rvest)
+library(stringr)
 
 #load 2015 data
 season <- agg_player_season(2015)
@@ -15,7 +16,7 @@ players <- season %>% select(name, playerid)
 
 #get roster, assign players to offense, defense, st
 roster.2016x <- getURL("https://raw.githubusercontent.com/brndngrhm/eagles/master/roster_2016.csv")
-roster <- as.data.frame(read.csv(text=roster.2016x, strip.white = T))
+roster <- as.data.frame(read.csv(text=roster.2016x, strip.white = T, stringsAsFactors = F))
 roster$mode <- "defense"
 roster$mode[roster$pos == "C"] <- "offense"
 roster$mode[roster$pos == "OG"] <- "offense"
@@ -29,3 +30,8 @@ roster$mode[roster$pos == "RB"] <- "offense"
 roster$mode[roster$pos == "k"] <- "st"
 roster$mode[roster$pos == "p"] <- "st"
 
+
+#example 
+season %>% filter(forced.fumbs > 0) %>% group_by(name) %>% 
+  summarise(forced.fumbs = sum(forced.fumbs))%>% 
+  ungroup() %>% arrange(desc(forced.fumbs))
